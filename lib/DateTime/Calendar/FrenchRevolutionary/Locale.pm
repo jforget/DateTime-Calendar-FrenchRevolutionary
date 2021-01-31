d9bbeed6d1c14112ebfc88bbcc7466554a1136bd
Jean Forget <JFORGET@cpan.org>
2021-01-31 20:16:08 +0100

    Migrating from rt.cpan.org to Github issues and pull requests.

--- lib/DateTime/Calendar/FrenchRevolutionary/Locale.pm
+++ lib/DateTime/Calendar/FrenchRevolutionary/Locale.pm
@@ -45,7 +45,26 @@ Please refer to parent module:
 Support for this module is provided via the datetime@perl.org email
 list. See L<https://lists.perl.org/> for more details.
 
-Please enter bug reports at L<https://rt.cpan.org/>
+Please   report  any   bugs   or  feature   requests   to  Github   at
+C<https://github.com/jforget/DateTime-Calendar-FrenchRevolutionary>,
+and create an issue or submit a pull request.
+
+If you have no  feedback after a week or so, try to  reach me by email
+at JFORGET  at cpan  dot org.  The notification  from Github  may have
+failed to reach  me. In your message, please  mention the distribution
+name in the subject, so my spam  filter and I will easily dispatch the
+email to the proper folder.
+
+On the other  hand, I may be  on vacation or away from  Internet for a
+good  reason. Do  not be  upset if  I do  not answer  immediately. You
+should write  me at a leasurely  rythm, about once per  month, until I
+react.
+
+If after about six  months or a year, there is  still no reaction from
+me, you can worry and start the CPAN procedure for module adoption.
+See L<https://groups.google.com/g/perl.module-authors/c/IPWjASwuLNs>
+L<https://www.cpan.org/misc/cpan-faq.html#How_maintain_module>
+and L<https://www.cpan.org/misc/cpan-faq.html#How_adopt_module>.
 
 =head1 AUTHOR
 
